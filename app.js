affba97b60bea64a91a052bb5642e593bf129596
IanGithungo01 <githungoian53@gmail.com>
2020-04-27 11:11:09 +0300

    finished all the routes

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express();
 app.use(express.json());
 
 
-const students = JSON.parse(fs.readFileSync(`${__dirname}/ students.json`));
+
+const students = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/students.json`)
+);
 
 app.get('/api/v1/students', (req, res) => {
 
@@ -15,6 +18,91 @@ app.get('/api/v1/students', (req, res) => {
         }
     });
 });
+
+app.get('/api/v1/students/:adm', (req, res) =>
+{
+    const adm = req.params.adm * 1;
+    const student = students.find((el) => el.admission === adm);
+
+    if (!student)
+    {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Student not found'
+        })
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            student
+        }
+    });
+})
+
+app.post('/api/v1/students', (req, res) =>
+{
+    const newAdm = students[students.length - 1].admission + 1;
+    const newStudent = Object.assign({ admission: newAdm }, req.body);
+
+    if (!req.body.name || !req.body.gender)
+    {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name or gender'
+        })
+    }
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            students:newStudent
+        }
+    })
+    
+})
+
+app.patch('/api/v1/students/:adm', (req, res) =>
+{
+    const adm = +req.params.adm;
+    const student = students.find((el) => el.admission === adm);
+    const updateStudent = Object.assign(student, req.body);
+
+    if (!student)
+    {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Student not found'
+        })
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            updateStudent
+        }
+    })
+
+})
+
+app.delete('/api/v1/students/:adm', (req, res) =>
+{
+    const adm = +req.params.adm;
+    const student = students.find((el) => el.admission === adm);
+
+    if (!student)
+    {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Student not found'
+        })
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
 
 const port = 3000;
 app.listen(port, () => {
